83e338acb2cbbd228ca7a64aa7415af1b08eaccb
Lucas Monteiro <lucasantoniomonteiro1@gmail.com>
2025-08-20 22:28:03 -0300

    Inclusão Exercicios 3 de Matriz e correção do Exercicio 2

--- Matriz/Exercicio2.js
+++ Matriz/Exercicio2.js
@@ -1,55 +1,55 @@
-    let matriz = []
-    let nomes = []
-    let soma
+let matriz = []
+let nomes = []
+let soma
 
-    for (let i = 0; i < 4; i++) {
-        matriz[i] = []
-        nomes[i] = prompt(`Digite o nome do ${i + 1}° vendedor:`)
-        for (let j = 0; j < 7; j++) {
-            matriz[i][j] = parseInt(Math.random()*1000)
-        }
-    }
-
-    console.log(matriz)
-
-    for (let i = 0; i < matriz.length; i++) {
-        soma = 0
-        for (let j = 0; j < matriz[i].length; j++) {
-            soma += matriz[i][j]
-        }
-        console.log(`A soma das vendas do(a) ${nomes[i]} é R$ ${soma}`)
-        
-    }
-
-
+for (let i = 0; i < 4; i++) {
+    matriz[i] = []
+    nomes[i] = prompt(`Digite o nome do ${i + 1}° vendedor:`)
     for (let j = 0; j < 7; j++) {
-        soma = 0
-        for (let i = 0; i < matriz.length; i++) {
-            soma += matriz[i][j]
-        }
-        console.log(`A soma das vendas do ${j + 1}° dia é R$ ${soma}`)
-        
+        matriz[i][j] = parseInt(Math.random()*1000)
     }
+}
 
-    let maiorVenda = matriz[0][0]
-    let posição = "[0] [0]"
-    for (let i = 0; i < matriz.length; i++) {
-        
-        for (let j = 0; j < matriz[i].length; j++) {
-            if (matriz[i][j] > maiorVenda) {
-                maiorVenda = matriz[i][j]
-                posição = `[${i}] [${j}]`
-            }
-        }
+console.log(matriz)
+
+for (let i = 0; i < matriz.length; i++) {
+    soma = 0
+    for (let j = 0; j < matriz[i].length; j++) {
+        soma += matriz[i][j]
     }
+    console.log(`A soma das vendas do(a) ${nomes[i]} é R$ ${soma}`)
+    
+}
 
-    console.log(`A maior venda foi de R$ ${maiorVenda} na posição ${posição}`)
 
+for (let j = 0; j < 7; j++) {
     soma = 0
     for (let i = 0; i < matriz.length; i++) {
-        for (let j = 0; j < matriz[i].length; j++) {
-            soma += matriz[i][j]
+        soma += matriz[i][j]
+    }
+    console.log(`A soma das vendas do ${j + 1}° dia é R$ ${soma}`)
+    
+}
+
+let maiorVenda = matriz[0][0]
+let posição = "[0] [0]"
+for (let i = 0; i < matriz.length; i++) {
+    
+    for (let j = 0; j < matriz[i].length; j++) {
+        if (matriz[i][j] > maiorVenda) {
+            maiorVenda = matriz[i][j]
+            posição = `[${i}] [${j}]`
         }
     }
-    console.log(`A média das vendas na semana foi de R$ ${(soma/7).toFixed(2)}`)
+}
+
+console.log(`A maior venda foi de R$ ${maiorVenda} na posição ${posição}`)
+
+soma = 0
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        soma += matriz[i][j]
+    }
+}
+console.log(`A média das vendas foi de R$ ${(soma/matriz.length * matriz[0].length).toFixed(2)}`)
 
---
+++ Matriz/Exercicio3.js
@@ -0,0 +1,76 @@
+let matriz = []
+let regiao = []
+let soma
+let maiorIndice
+let maiorlinhaColuna
+let maior300 = 0
+let media = []
+let menorRegiao
+let menor
+
+for (let i = 0; i < 5; i++) {
+    matriz[i] = []
+    for (let j = 0; j < 5; j++) {
+        matriz[i][j] = Number(prompt(`Digite o indice da regiao ${i} no dia ${j}:`))
+    }
+}
+
+console.log(matriz)
+
+console.log("Diagonal principal da matriz:")
+soma = 0
+for (let i = 0; i < matriz.length; i++) {
+    console.log(matriz[i][i])
+    soma += matriz[i][i]
+
+}
+console.log(`A média da diagonal principal é ${soma/matriz.length}`)
+
+console.log("Diagonal secundaria da matriz:")
+soma = 0
+for (let i = 0; i < matriz.length; i++) {
+    console.log(matriz[i][4 - i])
+    soma += matriz[i][4 - i]
+}
+console.log(`A média da diagonal secundaria é ${soma/matriz.length}`)
+
+maior = matriz[0][0]
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        if (matriz[i][j] > maior) {
+            maior = matriz[i][j]
+            maiorlinhaColuna = `Linha [i] coluna [j]`
+        }
+    }
+}
+
+console.log("Indices superior a 300")
+
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        if (matriz[i][j] > 300) {
+            maior300 += 1
+        }
+    }
+}
+console.log(`${maior300} indece(s) maior(s) que 300`)
+
+
+for (let i = 0; i < 5; i++) {
+    soma = 0
+    for (let j = 0; j < 5; j++) {
+        soma += matriz[i][j]
+    }
+    media[i] = soma/(matriz.length)
+}
+menor = media[0]
+menorRegiao = 0
+for (let i = 0; i < 5; i++) {
+    if (media[i] < menor) {
+        menor = media[i]
+        menorRegiao = i
+    }
+}
+console.log(`Médias por região: ${media}`)
+
+console.log(`A regiao com a menor media dos 5 dias foi a região ${menorRegiao} com a media de indices em ${menor}`)
